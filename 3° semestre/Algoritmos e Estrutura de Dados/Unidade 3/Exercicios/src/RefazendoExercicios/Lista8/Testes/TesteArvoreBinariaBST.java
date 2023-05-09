2e198bcc2ee3578c80e8f648c24f59974f4972aa
Rique25 <luiz.martendal52@gmail.com>
2023-05-08 21:35:20 -0300

    .

--- 3° semestre/Algoritmos e Estrutura de Dados/Unidade 3/Exercicios/src/RefazendoExercicios/Lista8/Testes/TesteArvoreBinariaBST.java
+++ 3° semestre/Algoritmos e Estrutura de Dados/Unidade 3/Exercicios/src/RefazendoExercicios/Lista8/Testes/TesteArvoreBinariaBST.java
@@ -42,10 +42,24 @@ public class TesteArvoreBinariaBST {
         Veiculo a = new Veiculo("MJT-7432", "Fusca", 1912, "Pedro");
         Veiculo b = new Veiculo("MIT-8008", "Celta", 1999, "Douglas");
         Veiculo c = new Veiculo("MEI-4665", "Gol", 2002, "Carlos");
+        Veiculo d = new Veiculo("MIO-6668", "Fusca", 1915, "Aline");
+        Veiculo e = new Veiculo("DIO-3333", "Celta", 1999, "Fausto");
+        Veiculo f = new Veiculo("PAO-6472", "C3", 2007, "Carolina");
+        Veiculo g = new Veiculo("CAO-2890", "C4", 2012, "Kelvin");
+        Veiculo h = new Veiculo("UVA-3939", "BMW", 2015, "Zé");
+        Veiculo i = new Veiculo("OVO-0000", "Gol", 2000, "Joshua");
+        Veiculo j = new Veiculo("ABC-1234", "Ferrari", 2015, "Marta");
 
         arvoreBST2.inserir(a);
         arvoreBST2.inserir(b);
         arvoreBST2.inserir(c);
+        arvoreBST2.inserir(d);
+        arvoreBST2.inserir(e);
+        arvoreBST2.inserir(f);
+        arvoreBST2.inserir(g);
+        arvoreBST2.inserir(h);
+        arvoreBST2.inserir(i);
+        arvoreBST2.inserir(j);
 
         System.out.println(arvoreBST2.toStringOrdered());
     }
