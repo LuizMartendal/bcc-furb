62817fa3aaf84997b3572d6cfca52e27a9e0d648
Rique25 <luiz.martendal52@gmail.com>
2023-08-10 17:58:09 -0300

    .

--- 4º Semestre/Programação Web 2/Prova de suficiência/RestApiFurb/src/main/java/br/furb/restapifurb/configurations/SecurityConfiguration.java
+++ 4º Semestre/Programação Web 2/Prova de suficiência/RestApiFurb/src/main/java/br/furb/restapifurb/configurations/SecurityConfiguration.java
@@ -1,26 +1,20 @@
 package br.furb.restapifurb.configurations;
 
-import br.furb.restapifurb.exceptions.ExceptionHandlerInterceptor;
 import br.furb.restapifurb.security.JwtTokenFilter;
 import br.furb.restapifurb.security.JwtTokenProvider;
 import br.furb.restapifurb.services.usuario.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.ExceptionHandlingConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.filter.OncePerRequestFilter;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
